133c2398f56c905244bbae439a48197d40b88f01
VvencesS <DucSoanVences99@gmail.com>
2020-11-24 22:32:21 +0700

    Template engine (.handlebars) - chia layout cho web (header, body, footer).

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars({
+    /* config */
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs'); 
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000);
