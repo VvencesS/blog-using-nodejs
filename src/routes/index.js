456d56110cddf51700006306e9e4daa296f9dcda
VvencesS <DucSoanVences99@gmail.com>
2020-11-30 23:32:10 +0700

    Tao route cho home, search (Site)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,11 @@
 const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 function route(app) {
-    app.get('/', (req, res) => {
-        res.render('home');
-    });
     
     app.use('/news', newsRouter);
+    app.use('/', siteRouter);
     
-    app.get('/search', (req, res) => {
-        res.render('search');
-    });
-    
-    app.post('/search', (req, res) => {
-        res.send('');
-    });
 }
 
 module.exports = route;
