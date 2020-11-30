d5abae64c1497c0a94298fd0d6740b4bb44f595b
VvencesS <DucSoanVences99@gmail.com>
2020-11-30 23:22:31 +0700

    Tao route cho news

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 // Conf thư mục file tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Apply middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -19,8 +27,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs'); 
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
+// routes init
+route(app);
 
-app.listen(3000);
+app.listen(port);
