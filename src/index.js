befbed83ca16be177a73fa622b811941fa93e44a
VvencesS <DucSoanVences99@gmail.com>
2020-12-01 23:22:04 +0700

    Install mongooses va viet ham connet tơi db

--- src/index.js
+++ src/index.js
@@ -6,25 +6,34 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// Connect DB
+db.connect();
 
 // Conf thư mục file tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Apply middleware
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({
+app.engine(
+  'hbs',
+  handlebars({
     /* config */
-    extname: '.hbs'
-}));
-app.set('view engine', 'hbs'); 
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // routes init
