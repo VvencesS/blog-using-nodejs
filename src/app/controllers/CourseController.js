2e2298000ca2e1dd341edf0dda46750ebbe5779d
VvencesS <DucSoanVences99@gmail.com>
2020-12-06 12:28:20 +0700

    Xay dung chuc năng xoa mem (soft delete) dung mongoose-delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -18,13 +18,12 @@ class CourseController {
 
   // [POST] /courses/store
   store(req, res, next) {
-    const formData = req.body;
-    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
-    const course = new Course(formData);
+    req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(req.body);
     course
       .save()
       .then(() => {
-        res.redirect('/');
+        res.redirect('/me/stored/courses');
       })
       .catch((error) => {
         console.log(error);
@@ -51,6 +50,20 @@ class CourseController {
 
   // [DELETE] /courses/:id
   delete(req, res, next) {
+    Course.delete({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
+  }
+
+  // [PATCH] /course/:id/restore
+  restore(req, res, next) {
+    Course.restore({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
+  }
+
+  // [DELETE] /courses/:id/force
+  forceDelete(req, res, next) {
     Course.deleteOne({ _id: req.params.id })
       .then(() => res.redirect('back'))
       .catch(next);
